297b7122c10b5c6f5c1a429a501cfd6ce79eb2e0
chandu-krishna <pillichanduksrishna@gmail.com>
2025-09-01 18:14:28 +0530

    Role Controller

--- Employee-Management-System-Backend/Program.cs
+++ Employee-Management-System-Backend/Program.cs
@@ -1,9 +1,16 @@
+using Employee_Management_System_Backend.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
+// Register RoleRepository as a singleton or scoped (recommended: scoped)
+builder.Services.AddScoped<RoleRepository>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+// Swagger/OpenAPI
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
