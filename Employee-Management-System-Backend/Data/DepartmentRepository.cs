a8d2339338f2da999f2485ff6d2d2110ccabf6bf
gangadharpepakayala <gangadharpepakayala22@gmail.com>
2025-09-08 12:02:29 +0530

    gangadhar

--- Employee-Management-System-Backend/Data/DepartmentRepository.cs
+++ Employee-Management-System-Backend/Data/DepartmentRepository.cs
@@ -1,6 +1,7 @@
 ﻿using Employee_Management_System_Backend.Model;
 using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -19,15 +20,20 @@ namespace Employee_Management_System_Backend.Data
         {
             var list = new List<Department>();
             using var conn = new SqlConnection(_conn);
-            using var cmd = new SqlCommand("SELECT Id, DepartmentName FROM Departments", conn);
+            using var cmd = new SqlCommand("SELECT * FROM Departments", conn);
             await conn.OpenAsync();
             using var reader = await cmd.ExecuteReaderAsync();
             while (await reader.ReadAsync())
             {
                 list.Add(new Department
                 {
-                    Id = reader.GetInt32(0),
-                    DepartmentName = reader.GetString(1)
+                    Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                    DepartmentName = reader.GetString(reader.GetOrdinal("DepartmentName")),
+                    Status = reader.GetBoolean(reader.GetOrdinal("Status")),
+                    CreatedBy = reader.IsDBNull(reader.GetOrdinal("CreatedBy")) ? null : reader.GetInt32(reader.GetOrdinal("CreatedBy")),
+                    CreatedDateTime = reader.GetDateTime(reader.GetOrdinal("CreatedDateTime")),
+                    UpdatedBy = reader.IsDBNull(reader.GetOrdinal("UpdatedBy")) ? null : reader.GetInt32(reader.GetOrdinal("UpdatedBy")),
+                    UpdatedDateTime = reader.IsDBNull(reader.GetOrdinal("UpdatedDateTime")) ? null : reader.GetDateTime(reader.GetOrdinal("UpdatedDateTime"))
                 });
             }
             return list;
@@ -37,7 +43,7 @@ namespace Employee_Management_System_Backend.Data
         {
             Department? dept = null;
             using var conn = new SqlConnection(_conn);
-            using var cmd = new SqlCommand("SELECT Id, DepartmentName FROM Departments WHERE Id=@id", conn);
+            using var cmd = new SqlCommand("SELECT * FROM Departments WHERE Id=@id", conn);
             cmd.Parameters.AddWithValue("@id", id);
             await conn.OpenAsync();
             using var reader = await cmd.ExecuteReaderAsync();
@@ -45,8 +51,13 @@ namespace Employee_Management_System_Backend.Data
             {
                 dept = new Department
                 {
-                    Id = reader.GetInt32(0),
-                    DepartmentName = reader.GetString(1)
+                    Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                    DepartmentName = reader.GetString(reader.GetOrdinal("DepartmentName")),
+                    Status = reader.GetBoolean(reader.GetOrdinal("Status")),
+                    CreatedBy = reader.IsDBNull(reader.GetOrdinal("CreatedBy")) ? null : reader.GetInt32(reader.GetOrdinal("CreatedBy")),
+                    CreatedDateTime = reader.GetDateTime(reader.GetOrdinal("CreatedDateTime")),
+                    UpdatedBy = reader.IsDBNull(reader.GetOrdinal("UpdatedBy")) ? null : reader.GetInt32(reader.GetOrdinal("UpdatedBy")),
+                    UpdatedDateTime = reader.IsDBNull(reader.GetOrdinal("UpdatedDateTime")) ? null : reader.GetDateTime(reader.GetOrdinal("UpdatedDateTime"))
                 };
             }
             return dept;
@@ -55,8 +66,15 @@ namespace Employee_Management_System_Backend.Data
         public async Task AddAsync(Department dept)
         {
             using var conn = new SqlConnection(_conn);
-            using var cmd = new SqlCommand("INSERT INTO Departments (DepartmentName) VALUES (@name)", conn);
+            using var cmd = new SqlCommand(
+                "INSERT INTO Departments (DepartmentName, Status, CreatedBy, CreatedDateTime) " +
+                "VALUES (@name, @status, @createdby, @createddatetime)", conn);
+
             cmd.Parameters.AddWithValue("@name", dept.DepartmentName);
+            cmd.Parameters.AddWithValue("@status", dept.Status);
+            cmd.Parameters.AddWithValue("@createdby", (object?)dept.CreatedBy ?? DBNull.Value);
+            cmd.Parameters.AddWithValue("@createddatetime", dept.CreatedDateTime);
+
             await conn.OpenAsync();
             await cmd.ExecuteNonQueryAsync();
         }
@@ -64,9 +82,17 @@ namespace Employee_Management_System_Backend.Data
         public async Task UpdateAsync(Department dept)
         {
             using var conn = new SqlConnection(_conn);
-            using var cmd = new SqlCommand("UPDATE Departments SET DepartmentName=@name WHERE Id=@id", conn);
+            using var cmd = new SqlCommand(
+                "UPDATE Departments " +
+                "SET DepartmentName=@name, Status=@status, UpdatedBy=@updatedby, UpdatedDateTime=@updateddatetime " +
+                "WHERE Id=@id", conn);
+
             cmd.Parameters.AddWithValue("@id", dept.Id);
             cmd.Parameters.AddWithValue("@name", dept.DepartmentName);
+            cmd.Parameters.AddWithValue("@status", dept.Status);
+            cmd.Parameters.AddWithValue("@updatedby", (object?)dept.UpdatedBy ?? DBNull.Value);
+            cmd.Parameters.AddWithValue("@updateddatetime", (object?)dept.UpdatedDateTime ?? DBNull.Value);
+
             await conn.OpenAsync();
             await cmd.ExecuteNonQueryAsync();
         }
